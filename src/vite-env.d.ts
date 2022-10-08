6a23343c8695ff5902e4c4a01ff5d23507bfdcc7
vaaski <admin@vaa.ski>
2022-10-08 10:41:04 +0200

    format with xo

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
+
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>
   export default component
 }
